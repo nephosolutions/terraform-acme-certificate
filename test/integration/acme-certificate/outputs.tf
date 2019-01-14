484fa04580a16e4b17217fe44cc7b057a9d02823
Sebastian Trebitz <sebastian@nephosolutions.com>
2019-01-14 21:46:44 +0100

    configure test-kitchen and CircleCI

---
+++ test/integration/acme-certificate/outputs.tf
@@ -0,0 +1,28 @@
+#   Copyright 2019 NephoSolutions SPRL, Sebastian Trebitz
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+output "certificate" {
+  description = "The requested ACME certificate"
+  value       = "${module.acme_certificate.certificate}"
+}
+
+output "private_key" {
+  description = "The requested ACME certificate private key"
+  value       = "${module.acme_certificate.private_key}"
+}
+
+output "fullchain" {
+  description = "The requested ACME certificate full chain"
+  value       = "${module.acme_certificate.fullchain}"
+}
---
+++ test/integration/acme-certificate/variables.tf
@@ -0,0 +1,26 @@
+#   Copyright 2019 NephoSolutions SPRL, Sebastian Trebitz
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+variable "dns_challenge" {
+  description = "The [DNS challenge|https://www.terraform.io/docs/providers/acme/r/certificate.html#using-dns-challenges] to use in fulfilling the request."
+  type = "map"
+}
+
+variable "dns_domain" {
+  description = "The DNS domain used to provision the test certificates with the given dns_challenge provider."
+}
+
+variable "email_address" {
+  description = "The contact email address for the account."
+}
